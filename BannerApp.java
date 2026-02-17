d65a23aeca9a018819ac3d9823437505c33bd9d0
Raj Pratap <raj@Rajs-MacBook-Air-2.local>
2026-02-17 14:54:00 +0530

    UC1: Print OOPS to console

--- BannerApp.java
+++ BannerApp.java
@@ -1,5 +1,5 @@
 public class BannerApp {
     public static void main(String[] args) {
-        System.out.println("🚀 Welcome to OopsBannerApp Project!");
+        System.out.println("OOPS");
     }
 }
