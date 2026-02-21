a7a74f0d46fcd4ee0b88e6636c45db79727a0288
Raj Pratap <raj@Rajs-MacBook-Air-2.local>
2026-02-21 15:10:03 +0530

    Added UC4 Array and Loops banner

---
+++ UC4_ArrayLoopsBanner.java
@@ -0,0 +1,22 @@
+public class UC4_ArrayLoopsBanner {
+
+    public static void main(String[] args) {
+
+        String banner[] = {
+
+        " *****   *****   *****   ***** ",
+        " *   *   *   *   *   *   *     ",
+        " *   *   *   *   *   *   *     ",
+        " *   *   *****   *****   ***** ",
+        " *   *   *       *           * ",
+        " *   *   *       *           * ",
+        " *****   *       *       ***** "
+
+        };
+
+        for(String line : banner) {
+            System.out.println(line);
+        }
+
+    }
+}
